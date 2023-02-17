7ae0ab8a304d80bfd49490224fbadd5c77a44219
DushyantBhardwaj22 <maniebhardwaj1@gmail.com>
2023-02-18 02:15:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState, useEffect} from "react"
+import Clock from "./Clock"
+import "./App.css"
+function App(){
+  const [timer,setTImer]=useState(0)
+  const handleTimer=()=>{
+    const value = Math.floor(document.getElementById("time").value)
+    if(isNaN(value)){
+      setTImer(0)
 
-function App() {
+    }else{
+      setTImer(value)
+    }
+  }
+  useEffect(()=>{
+    const interval = setInterval(()=>{
+      setTImer(timer=>timer-1)
+    },1000)
+    return ()=> clearInterval(interval)
+    
+  },[])
+  useEffect(()=>{
+    if(timer<=0){
+      clearInterval(setTImer(0))
+      return
+      
+    }
+  },[timer])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className="heading">CountDown Timer</h1>
+      <input type="text" placeholder="enter time in seconds" id="time" onChange={handleTimer}/>
+      <Clock timer={timer}/>
     </div>
-  );
-}
-
-export default App;
+  )
+} 
+export default App
---
+++ src/Clock.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+
+const Clock=({ timer}) => {
+    return(
+            <section className="container">
+                <section className="time">
+                    <div className="Clock">
+                    <section>
+                        <p id="shownTime">{timer}</p>
+                        <small className="sechead">Seconds</small>
+                    </section>
+                    </div>
+                </section>
+            </section>
+      
+    )
+}
+Clock.defaultProps={
+    timer: 0,
+}
+export default Clock
